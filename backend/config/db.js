cc9947a43860cf7affa65744e89a9cdf89b35654
Nguyễn Hoài Bảo <22110108@student.hcmute.edu.vn>
2025-10-04 14:45:49 +0700

    connect db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://ngyyennhoaiiibaoo:baoproviplun2015%40@ngyyennhoaiiibaoo.o6hnwf3.mongodb.net/ProjectFlow")
+    await mongoose.connect(process.env.CONNECTION_STRING)
         .then(() => console.log("Connected to MongoDB"))
         .catch((err) => console.log(err));}
